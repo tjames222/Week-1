5ed92f2e09002603936f5ab15dfa354078800fa8
Tim James <32233010+tjames222@users.noreply.github.com>
2017-10-04 15:28:00 -0700

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author tjames
+ */
+public class Introduction {
+    public static void main(String[] args)  {
+        char[] myName = { 'T','i','m' };
+        String name = new String(myName); 
+        String color = "red";
+        String winLottery = "invest in donuts!";
+        
+        System.out.println("My name is " + name);
+        System.out.println("My favorite color is " + color);
+        System.out.println("If I won the lottery, I would " + winLottery);   
+    }
+}
